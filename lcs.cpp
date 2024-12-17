8a343e8ed8c722baffa7f76f78724dbcdd978dae
Aryan Rastogi <aryanrastogi@Mac.lan>
2024-12-17 20:08:37 +0530

    practice

--- lcs.cpp
+++ lcs.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int lcs(string s, string t, int m, int n) {
-  if (m == 0 || n == 0) return "";
+  if (m == 0 || n == 0) return 0;
+  return 1;
 }
 
 int main() {
